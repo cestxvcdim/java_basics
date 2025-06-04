d7dfe795ebd235eefbd673852d6b629198f6188b
vvvadim <v.vladimirov@it-park.tech>
2025-06-04 16:51:47 +0300

    hw4

--- src/Main.java
+++ src/Main.java
@@ -1,99 +1,73 @@
 public class Main {
     public static void main(String[] args) {
-        // task 1
-        byte age = 19;
-        if (age >= 18) {
-            System.out.println("Совершенолетний");
-        }
-        else {
-            System.out.println("Несовершенолетний");
-        }
+        // task 1, 2
+        byte clientOS = 0;
+        short clientDeviceYear = 2015;
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015)
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
 
-        // task 2
-        byte temperature = 2;
-        if (temperature >= 5) {
-            System.out.println("Сегодня тепло, можно идти без шапки");
+            else System.out.println("Установите версию приложения для iOS по ссылке");
         }
         else {
-            System.out.println("На улице холодно, нужно надеть шапку");
+            if (clientDeviceYear < 2015)
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+
+            else System.out.println("Установите версию приложения для Android по ссылке");
         }
 
         // task 3
-        short speed = 55;
-        if (speed > 60) {
-            System.out.println("Скорость превышена");
+        short year = 2025;
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
         }
-        else {
-            System.out.println("Скорость не превышена");
-        }
+        else System.out.println(year + " год не является високосным");
 
         // task 4
-        if (age >= 2 && age < 7) {
-            System.out.println("Пора в детский сад");
+        short deliveryDistance = 95;
+        short days = 1;
+        if (deliveryDistance >= 0 && deliveryDistance < 20) {
+            System.out.println("Потребуется дней: " + days);
         }
-        else if (age >= 7 && age < 18) {
-            System.out.println("Пора в школу");
+        else if (deliveryDistance < 60) {
+            days += 1;
+            System.out.println("Потребуется дней: " + days);
         }
-        else if (age >= 18 && age <= 24) {
-            System.out.println("Пора в универ");
+        else if (deliveryDistance < 100) {
+            days += 1;
+            System.out.println("Потребуется дней: " + days);
         }
-        else if (age >= 24) {
-            System.out.println("Пора на работу");
-        }
+        else System.out.println("Доставки нет");
 
         // task 5
-        if (age < 5) {
-            System.out.println("Нельзя кататься на аттракционе");
-        }
-        else if (age >= 5 && age < 14) {
-            System.out.println("Можно кататься на аттракционе, но только со взрослым");
-        }
-        else if (age >= 14) {
-            System.out.println("Можно кататься на аттракционе, даже без взрослого");
-        }
-
-        // task 6
-        byte capacity = 102;
-        byte sitPlaces = 60;
-        byte standPlaces = (byte) (capacity - sitPlaces);
-
-        byte sitPlacesBusy = 34;
-        byte standPlacesBusy = 57;
-        if (sitPlacesBusy < sitPlaces) {
-            System.out.println("Есть сидячее место, чувак");
-        }
-        else if (standPlacesBusy < standPlaces) {
-            System.out.println("Есть стоячее место, чувак");
-        }
-        else {
-            System.out.println("Все занято, чувак");
-        }
-
-        // task 7
-        int one = 14;
-        int two = 15;
-        int three = 15;
-
-        if (one > two && one > three) {
-            System.out.println("one - большее число: " + one);
-        }
-        else if (two > one && two > three) {
-            System.out.println("two - большее число: " + two);
-        }
-        else if (three > one && three > two) {
-            System.out.println("three - большее число: " + three);
-        }
-        else if (one < two && one < three) {
-            System.out.println("two и three - большие числа: " + two);
-        }
-        else if (two < one && two < three) {
-            System.out.println("one и three - большие числа: " + one);
-        }
-        else if (three < one && three < two) {
-            System.out.println("one и two - большие числа: " + one);
-        }
-        else {
-            System.out.println("Все числа равны, это число: " + one);
+        short month = 1;
+        switch (month) {
+            case 1:
+                System.out.println("Январь");
+            case 2:
+                System.out.println("Февраль");
+            case 3:
+                System.out.println("Март");
+            case 4:
+                System.out.println("Апрель");
+            case 5:
+                System.out.println("Май");
+            case 6:
+                System.out.println("Июнь");
+            case 7:
+                System.out.println("Июль");
+            case 8:
+                System.out.println("Август");
+            case 9:
+                System.out.println("Сентябрь");
+            case 10:
+                System.out.println("Октябрь");
+            case 11:
+                System.out.println("Ноябрь");
+            case 12:
+                System.out.println("Декабрь");
+            default:
+                System.out.println("Ошибка");
         }
     }
 }
