1542161b1d60b602ced45319283a4a316d0439c3
vvvadim <v.vladimirov@it-park.tech>
2025-06-09 00:41:40 +0300

    hw10

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,55 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         // task 1
-        int [] arr1 = new int[] {1, 2, 3};
-        double [] arr2 = {1.57, 7.654, 9.986};
-        char [] myArr = {'a', 'b', 'c'};
+        int currentYear = LocalDate.now().getYear();
+        isLeapYear(currentYear);
 
         // task 2
-        for (int i = 0; i < arr1.length; i++) {
-            if (i == arr1.length - 1) {
-                System.out.print(arr1[i]);
-            }
-            else System.out.printf("%s, ", arr1[i]);
-        }
-        System.out.println();
-
-        for (int i = 0; i < arr2.length; i++) {
-            if (i == arr2.length - 1) {
-                System.out.print(arr2[i]);
-            }
-            else System.out.printf("%s, ", arr2[i]);
-        }
-        System.out.println();
-
-        for (int i = 0; i < myArr.length; i++) {
-            if (i == myArr.length - 1) {
-                System.out.print(myArr[i]);
-            }
-            else System.out.printf("%s, ", myArr[i]);
-        }
-        System.out.println();
+        getInstallMessage(0, currentYear);
 
         // task 3
-        for (int i = arr1.length - 1; i >= 0; i--) {
-            if (i == 0) {
-                System.out.print(arr1[i]);
-            }
-            else System.out.printf("%s, ", arr1[i]);
-        }
-        System.out.println();
+        getDeliveryMessage(95);
 
-        for (int i = arr2.length - 1; i >= 0; i--) {
-            if (i == 0) {
-                System.out.print(arr2[i]);
-            }
-            else System.out.printf("%s, ", arr2[i]);
-        }
-        System.out.println();
+    }
 
-        for (int i = myArr.length - 1; i >= 0; i--) {
-            if (i == 0) {
-                System.out.print(myArr[i]);
-            }
-            else System.out.printf("%s, ", myArr[i]);
+    public static void isLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
         }
-        System.out.println();
+        else System.out.println(year + " год не является високосным");
+    }
 
-        // task 4
-        for (int i = 0; i < arr1.length; i++) {
-            if (arr1[i] % 2 != 0) arr1[i]++;
-            System.out.printf("%s ", arr1[i]);
+    public static void getInstallMessage(int clientOS, int clientDeviceYear) {
+        String outMessage = "Установите%s версию приложения для %s по ссылке\n";
+        if (clientOS == 0 && clientDeviceYear < 2015) {
+            System.out.printf(outMessage, " облегчённую", "iOS");
         }
+        else if (clientOS == 0) {
+            System.out.printf(outMessage, "", "iOS");
+        }
+        else if (clientOS == 1 && clientDeviceYear < 2015) {
+            System.out.printf(outMessage, " облегчённую", "Android");
+        }
+        else if (clientOS == 1) {
+            System.out.printf(outMessage, "", "Android");
+        }
+    }
+
+    public static void getDeliveryMessage(int deliveryDistance) {
+        short days = 1;
+        if (deliveryDistance >= 0 && deliveryDistance < 20) {
+            System.out.println("Потребуется дней: " + days);
+        }
+        else if (deliveryDistance < 60) {
+            days += 1;
+            System.out.println("Потребуется дней: " + days);
+        }
+        else if (deliveryDistance < 100) {
+            days += 1;
+            System.out.println("Потребуется дней: " + days);
+        }
+        else System.out.println("Доставки нет");
     }
 }
