d1c27e0b3cf5b9049874cf56755225238777c897
vvvadim <v.vladimirov@it-park.tech>
2025-06-12 01:25:53 +0300

    hw11

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,68 @@
-import java.util.Arrays;
+import java.time.LocalDate;
+
+class Author {
+    private String name;
+    private String surname;
+
+    Author(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getSurname() {
+        return surname;
+    }
+}
+
+class Book {
+    private String title;
+    private Author author;
+    private int releaseYear;
+
+    Book(String title, Author author, int releaseYear) {
+        this.title = title;
+        this.author = author;
+        this.releaseYear = releaseYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public Author getAuthor() {
+        return author;
+    }
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        if (releaseYear <= LocalDate.now().getYear() && releaseYear >= 0) {
+            this.releaseYear = releaseYear;
+        }
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
-        // task 1
-        int [] arr1 = {10000, 12000, 5000, 30000, 27500};
-        int sum = Arrays.stream(arr1).sum();
-        System.out.printf("Сумма трат за месяц составила %s рублей\n", sum);
+        Author gosling = new Author("Райяан", "Гослинг");
+        Author noname = new Author("Ноунейм", "Какой-то");
 
-        // task 2
-        int [] arr2 = {2000, 3500, 1400, 6900, 4760};
-        int minSpend = Arrays.stream(arr2).min().getAsInt();
-        int maxSpend = Arrays.stream(arr2).max().getAsInt();
-        System.out.printf("Минимальная сумма трат за неделю составила %s рублей.\n", minSpend);
-        System.out.printf("Максимальная сумма трат за неделю составила %s рублей.\n", maxSpend);
+        Book modernMasterpiece = new Book("Шедевр современности", gosling, 2025);
+        Book hackneyedClassic = new Book("Избитая классика", noname, 1978);
 
-        // task 3
-        double [] arr3 = {23000, 16700, 7750, 14000, 17800};
-        double average = Arrays.stream(arr3).sum() / arr3.length;
-        System.out.printf("Средняя сумма трат за месяц составила %s рублей\n", average);
+        System.out.println(modernMasterpiece.getTitle());
+        System.out.println(hackneyedClassic.getTitle());
 
-        // task 4
-        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = 0; i < reverseFullName.length / 2; i++) {
-            char temp = reverseFullName[i];
-            reverseFullName[i] = reverseFullName[reverseFullName.length - i - 1];
-            reverseFullName[reverseFullName.length - i - 1] = temp;
-        }
-        for (char c: reverseFullName) {
-            System.out.print(c);
-        }
+        System.out.println(modernMasterpiece.getReleaseYear());
+        System.out.println(hackneyedClassic.getReleaseYear());
+
+        System.out.println(modernMasterpiece.getAuthor().getSurname());
+        System.out.println(hackneyedClassic.getAuthor().getName());
+
+        hackneyedClassic.setReleaseYear(1964);
+        System.out.println(hackneyedClassic.getReleaseYear());
     }
 }
